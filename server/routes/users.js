a12a20fc7f5186625bbe0979f57c44905b9e385c
Shubham <thorveshubham@gmail.com>
2020-06-07 14:49:41 +0530

    added auth

--- server/routes/users.js
+++ server/routes/users.js
@@ -9,7 +9,7 @@ router.post('/create',userController.createUser);
 router.post('/login',userController.login);
 router.post('/forgotPassword',userController.sendMail);
 router.post('/changePassword',userController.changePassword);
-router.get('/getUsers',auth,userController.getUsers);
+router.post('/getUsers',auth,userController.getUsers);
 
 
 module.exports = router;
